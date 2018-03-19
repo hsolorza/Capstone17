1642653737d57c43b069ca60a4c09b0fadf09069
shariffn <shariffn@oregonstate.edu>
2018-03-19 15:17:05 -0700

    Add files via upload

---
+++ Project_Implementation_Code/iCreate Menus/ScaleObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class ScaleObject : MonoBehaviour {
+
+    public Slider sliderLength;
+    public Slider sliderWidth;
+    public Slider sliderHeight;
+
+    //public Transform prefab;
+
+    private float X = 0f;
+    private float Y = 0f;
+    private float Z = 0f;
+
+    // Use this for initialization
+    void Start () {
+        //sliderLength.value = 1;
+        //sliderWidth.value = 1;
+        //sliderHeight.value = 1;
+    }
+    
+
+	
+	// Update is called once per frame
+	public void Scale () {
+
+        X = sliderLength.value;
+        Y = sliderHeight.value;
+        Z = sliderWidth.value;
+
+        transform.localScale = new Vector3(X, Y, Z);
+	}
+}
---
+++ Project_Implementation_Code/iCreate Menus/SpawnObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnObject : MonoBehaviour {
+
+    public Button myButton;
+
+    public Transform prefab;
+
+    // Use this for initialization
+    void Start () {
+        Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//
+	}
+
+    public void TaskOnClick()
+    {
+        Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+
+}
+
+
+
+
+
