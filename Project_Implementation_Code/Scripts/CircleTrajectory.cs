04911d59f6c28010f09367dbbe57d5d4472b7222
hsolorza <solorzah@oregonstate.edu>
2018-03-19 15:12:00 -0700

    Add circle ellipse parabola trajectory

---
+++ Project_Implementation_Code/Scripts/CircleTrajectory.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CircleTrajectory : MonoBehaviour
+{
+    public int segments = 60;
+    public float xoffset = 0;
+    public float yoffset = 0;
+    public float shrink = 1;
+    public float xradius;
+    public float yradius;
+    public List<GameObject> cube;
+    private LineRenderer line;
+
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.positionCount = (segments + 1);
+        line.useWorldSpace = false;
+        CreatePoints();
+    }
+
+
+    void CreatePoints()
+    {
+        cube = new List<GameObject>(segments);
+        GameObject tempCube;
+        float x;
+        float y;
+        float z = 0f;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            // circle
+            //x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius + xoffset;
+            //y = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius + yoffset;
+            //prabola
+            x = i - segments/2 + xoffset;
+            y = x*x*shrink + yoffset;   // a/2 t^2 + v0t + xdisp
+            line.SetPosition(i, new Vector3(x, y, z));
+            Debug.Log(line.GetPosition(i).x);
+            tempCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //tempCube.AddComponent<Rigidbody>();
+            tempCube.transform.position = new Vector3(line.GetPosition(i).x, line.GetPosition(i).y, line.GetPosition(i).z);
+            cube.Insert(i, tempCube);
+            angle += (360f / segments);
+        }
+    }
+}
---
+++ Project_Implementation_Code/Scripts/EllipseTrajectory.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class DrawCircle : MonoBehaviour
+{
+    public enum Axis { X, Y, Z };
+
+    [SerializeField]
+    [Tooltip("The number of lines that will be used to draw the circle. The more lines, the more the circle will be \"flexible\".")]
+    [Range(0, 1000)]
+    private int segments = 60;
+
+    [SerializeField]
+    [Tooltip("The radius of the horizontal axis.")]
+    private float horizRadius = 10;
+
+    [SerializeField]
+    [Tooltip("The radius of the vertical axis.")]
+    private float vertRadius = 10;
+
+    [SerializeField]
+    [Tooltip("The offset will be applied in the direction of the axis.")]
+    private float offset = 0;
+
+    [SerializeField]
+    [Tooltip("The axis about which the circle is drawn.")]
+    private Axis axis = Axis.Z;
+
+    [SerializeField]
+    [Tooltip("If checked, the circle will be rendered again each time one of the parameters change.")]
+    private bool checkValuesChanged = true;
+
+    private int previousSegmentsValue;
+    private float previousHorizRadiusValue;
+    private float previousVertRadiusValue;
+    private float previousOffsetValue;
+    private Axis previousAxisValue;
+
+    private LineRenderer line;
+
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.SetVertexCount(segments + 1);
+        line.useWorldSpace = false;
+
+        UpdateValuesChanged();
+
+        CreatePoints();
+    }
+
+    void Update()
+    {
+        if (checkValuesChanged)
+        {
+            if (previousSegmentsValue != segments ||
+                previousHorizRadiusValue != horizRadius ||
+                previousVertRadiusValue != vertRadius ||
+                previousOffsetValue != offset ||
+                previousAxisValue != axis)
+            {
+                CreatePoints();
+            }
+
+            UpdateValuesChanged();
+        }
+    }
+
+    void UpdateValuesChanged()
+    {
+        previousSegmentsValue = segments;
+        previousHorizRadiusValue = horizRadius;
+        previousVertRadiusValue = vertRadius;
+        previousOffsetValue = offset;
+        previousAxisValue = axis;
+    }
+
+    void CreatePoints()
+    {
+
+        if (previousSegmentsValue != segments)
+        {
+            line.SetVertexCount(segments + 1);
+        }
+
+        float x;
+        float y;
+        float z = offset;
+
+        float angle = 0f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * horizRadius;
+            y = Mathf.Cos(Mathf.Deg2Rad * angle) * vertRadius;
+
+            switch (axis)
+            {
+                case Axis.X:
+                    line.SetPosition(i, new Vector3(z, y, x));
+                    break;
+                case Axis.Y:
+                    line.SetPosition(i, new Vector3(y, z, x));
+                    break;
+                case Axis.Z:
+                    line.SetPosition(i, new Vector3(x, y, z));
+                    break;
+                default:
+                    break;
+            }
+
+            angle += (360f / segments);
+        }
+    }
+}
