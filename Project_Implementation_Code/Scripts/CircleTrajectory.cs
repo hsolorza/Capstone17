eadf8c28607039a7f0f1a9e65595fc0e9ab00620
hsolorza <solorzah@oregonstate.edu>
2018-03-21 14:47:23 -0700

    Complete the base circle

--- Project_Implementation_Code/Scripts/CircleTrajectory.cs
+++ Project_Implementation_Code/Scripts/CircleTrajectory.cs
@@ -1,13 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 
 public class CircleTrajectory : MonoBehaviour
 {
-    public int segments = 60;
-    public float xoffset = 0;
-    public float yoffset = 0;
+    private int segments = 60;
+    public float objectWidth = 1;
+    public float xoffset = 5;
+    public float yoffset = 5;
+    public float zoffset = 0;
     public float shrink = 1;
     public float xradius;
     public float yradius;
@@ -16,6 +18,7 @@ public class CircleTrajectory : MonoBehaviour
 
     void Start()
     {
+        segments = (int)(360 / (Mathf.Asin(objectWidth / (xradius)) * 180.0 / 3.1415));
         line = gameObject.GetComponent<LineRenderer>();
 
         line.positionCount = (segments + 1);
@@ -26,6 +29,7 @@ public class CircleTrajectory : MonoBehaviour
 
     void CreatePoints()
     {
+        
         cube = new List<GameObject>(segments);
         GameObject tempCube;
         float x;
@@ -36,17 +40,18 @@ public class CircleTrajectory : MonoBehaviour
 
         for (int i = 0; i < (segments + 1); i++)
         {
-            // circle
-            //x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius + xoffset;
-            //y = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius + yoffset;
-            //prabola
-            x = i - segments/2 + xoffset;
-            y = x*x*shrink + yoffset;   // a/2 t^2 + v0t + xdisp
+         
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius + xoffset;
+            y = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius + yoffset;
+
             line.SetPosition(i, new Vector3(x, y, z));
             Debug.Log(line.GetPosition(i).x);
             tempCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             //tempCube.AddComponent<Rigidbody>();
             tempCube.transform.position = new Vector3(line.GetPosition(i).x, line.GetPosition(i).y, line.GetPosition(i).z);
+            float xAngle = Mathf.Acos((line.GetPosition(i).x - xoffset) / xradius) * (float)(180.0 / 3.1415) * (line.GetPosition(i).y - xoffset)/Mathf.Abs((line.GetPosition(i).y - xoffset));
+            tempCube.transform.localEulerAngles = new Vector3(0,0,xAngle);
+            
             cube.Insert(i, tempCube);
             angle += (360f / segments);
         }
