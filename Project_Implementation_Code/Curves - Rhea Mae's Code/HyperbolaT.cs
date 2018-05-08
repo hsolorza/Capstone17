32c32d78b60caade80f241e40583e270810d632c
edwardrh <rhea.mae.167@gmail.com>
2018-05-08 16:32:26 -0700

    Edited parabola code, added hyperbola code for curves

--- Project_Implementation_Code/Curves - Rhea Mae's Code/HyperbolaT.cs
+++ Project_Implementation_Code/Curves - Rhea Mae's Code/HyperbolaT.cs
@@ -3,23 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class ParabolaT : MonoBehaviour
+public class HyperbolaT : MonoBehaviour
 
-// Equation for an Parabola (Vertex Form)
-// y = a(x-h)^2 + k
+// Equation for an Hyperbola (Vertex Form)
+// 1 = (((x-h)^2)/a^2) - (((y-k)^2)/b^2)
 
 {
-	// Number of times object in placed on Parabola
+	// Number of times object in placed on Hyperbola
     private int segments = 60;
 	// Width of the Object
     public float objectWidth = 1;
 	// User Input Variables
-    public float xoffset = 5; // h or x0 of equation
-    public float yoffset = 5; // v or y0 of equation
+    public float xoffset = 5; // h of equation
+    public float yoffset = 5; // v of equation
     public float zoffset = 0;
     public float shrink = 1;
-	// x-coefficient of the Parabola
-    public float a; // a or p of equation
+	// Asymptotes of the Hyperbola
+    public float xradius; // a of equation
+    public float yradius; // b of equation
 	// List of Objects
     public List<GameObject> cube;
 	// Some Variable
@@ -27,14 +28,14 @@ public class ParabolaT : MonoBehaviour
 
     void Start()
     {
-		// Calculating number of spaces an object will be placed at on Parabola
+		// Calculating number of spaces an object will be placed at on Hyperbola
 		// segments = (360) / (asin(objectWidth/radius)) * 180/3.1415 
         segments = (int)(360 / (Mathf.Asin(objectWidth / (xradius)) * 180.0 / 3.1415));
         line = gameObject.GetComponent<LineRenderer>();
         line.positionCount = (segments + 1);
         line.useWorldSpace = false;
 		
-		// Calling function to place objects along Parabola
+		// Calling function to place objects along Hyperbola
         CreatePoints();
     }
 
@@ -45,22 +46,22 @@ public class ParabolaT : MonoBehaviour
         cube = new List<GameObject>(segments);
 		// Creating a GameObject for the object being used
         GameObject tempCube;
-		// Physical variables to use to define each object along the Parabola
+		// Physical variables to use to define each object along the Hyperbola
         float x; // x of equation
         float y; // y of equation
         float z = 0f;
 		// Starting angle for the object
         float angle = 20f;
 
-		// Loop going through Parabola based off number of segments calculated earlier
+		// Loop going through Hyperbola based off number of segments calculated earlier
         for (int i = 0; i < (segments + 1); i++)
         {
-			// Calculating x and y of an object based off angle and radius of the Parabola
-			// x = a*sin(angle) + xoffset
-			// y = a*cos(angle) + yoffset
+			// Calculating x and y of an object based off angle and radius of the Hyperbola
+			// x = a*sec(angle) + xoffset = a*(1/cos(angle)) + xoffset
+			// y = b*tan(angle) + yoffset
 			// NOTE: Might have to be flipped like for the CircleT code, still unsure exactly why
-            x = xradius * Mathf.Sin(Mathf.Deg2Rad * angle) *  + xoffset;
-            y = yradius * Mathf.Cos(Mathf.Deg2Rad * angle) + yoffset;
+            x = xradius * (1 / Mathf.Cos(Mathf.Deg2Rad * angle)) *  + xoffset;
+            y = yradius * Mathf.Tan(Mathf.Deg2Rad * angle) + yoffset;
 
 			// Setting position calculated
             line.SetPosition(i, new Vector3(x, y, z));
@@ -74,7 +75,7 @@ public class ParabolaT : MonoBehaviour
 			// Placing object at calculated position
             tempCube.transform.position = new Vector3(line.GetPosition(i).x, line.GetPosition(i).y, line.GetPosition(i).z);
             
-			// Calculating angle of the object of the Parabola
+			// Calculating angle of the object of the Hyperbola
 			// angle = acos((x-xoffset)/(m))
 			// m = -(x-xoffset) / (y-yoffset)
 			float xAngle = Mathf.Acos((line.GetPosition(i).x - xoffset) / (-((line.GetPosition(i).x - xoffset) / (line.GetPosition(i).y - yoffset)))) * (float)(180.0 / 3.1415);
