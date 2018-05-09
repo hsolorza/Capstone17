3164dd0113cfb1504a61801c80b361556c7ad1e0
edwardrh <rhea.mae.167@gmail.com>
2018-05-08 21:10:15 -0700

    Updated curves - circle, ellipse

--- Project_Implementation_Code/Curves - Rhea Mae's Code/CircleT.cs
+++ Project_Implementation_Code/Curves - Rhea Mae's Code/CircleT.cs
@@ -1,83 +1,90 @@
-using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
+public class CircleT : MonoBehaviour {
 
-public class CircleT : MonoBehaviour
-{
-	// Number of times object in placed on Circle
+    // Number of times object in placed on Circle
     private int segments = 60;
-	// Width of the Object
+    // Width of the Object
     public float objectWidth = 1;
-	// User Input Variables
+    // User Input Variables
     public float xoffset = 5;
     public float yoffset = 5;
     public float zoffset = 0;
     public float shrink = 1;
-	// Radius of the Circle
+    // Radius of the Circle
     public float xradius;
     public float yradius;
-	// List of Objects
+    // List of Objects
     public List<GameObject> cube;
-	// Some Variable
+    // Some Variable
     private LineRenderer line;
 
-    void Start()
-    {
-		// Calculating number of spaces an object will be placed at on circle
-        segments = (int)(360 / (Mathf.Asin(objectWidth / (xradius)) * 180.0 / 3.1415));
+    // Use this for initialization
+    void Start () {
+
+        // Calculating number of spaces an object will be placed at on circle
+        segments = (int)(360 / (Mathf.Asin(objectWidth / (xradius)) * (180.0 / 3.1415)));
+        Debug.Log("Segments: " + segments);
+
         line = gameObject.GetComponent<LineRenderer>();
         line.positionCount = (segments + 1);
         line.useWorldSpace = false;
-		
-		// Calling function to place objects along circle
+
+        // Calling function to place objects along circle
         CreatePoints();
+
     }
 
+    void CreatePoints() {
 
-    void CreatePoints()
-    {
         // Create a new list for objects
         cube = new List<GameObject>(segments);
-		// Creating a GameObject for the object being used
+        // Creating a GameObject for the object being used
         GameObject tempCube;
-		// Physical variables to use to define each object along the circle
+        // Physical variables to use to define each object along the circle
         float x;
         float y;
         float z = 0f;
-		// Initial angle for the object
+        // Initial angle for the object
         float angle = 20f;
 
-		// Loop going through circle based off number of segments calculated earlier
+        // Loop going through circle based off number of segments calculated earlier
         for (int i = 0; i < (segments + 1); i++)
         {
-			// Calculating x and y of an object based off angle and radius of the circle
+            // Calculating x and y of an object based off angle and radius of the circle
             x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius + xoffset;
             y = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius + yoffset;
 
-			// Setting position calculated
+            // Setting position calculated
             line.SetPosition(i, new Vector3(x, y, z));
-			// Printing out x position of object
+            // Printing out x position of object
             Debug.Log(line.GetPosition(i).x);
-			
-			// Creating a cube as the GameObject being used
+
+            // Creating a cube as the GameObject being used
             tempCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             /*tempCube.AddComponent<Rigidbody>();*/
-			
-			// Placing object at calculated position
+
+            // Placing object at calculated position
             tempCube.transform.position = new Vector3(line.GetPosition(i).x, line.GetPosition(i).y, line.GetPosition(i).z);
-            
-			// Calculating angle of the object of the circle
-			float xAngle = Mathf.Acos((line.GetPosition(i).x - xoffset) / xradius) * (float)(180.0 / 3.1415) * (line.GetPosition(i).y - xoffset)/Mathf.Abs((line.GetPosition(i).y - xoffset));
-            
-			// Rotating object based off calculated angle from earlier
-			tempCube.transform.localEulerAngles = new Vector3(0,0,xAngle);
-            
-			// Placing cube..?
+
+            // Calculating angle of the object of the circle
+            float xAngle = Mathf.Acos((line.GetPosition(i).x - xoffset) / xradius) * (float)(180.0 / 3.1415) * (line.GetPosition(i).y - xoffset) / Mathf.Abs((line.GetPosition(i).y - xoffset));
+
+            // Rotating object based off calculated angle from earlier
+            tempCube.transform.localEulerAngles = new Vector3(0, 0, xAngle);
+
+            // Placing cube..?
             cube.Insert(i, tempCube);
-			
-			// Calculating next angle based off an additional segment calculated from earlier
+
+            // Calculating next angle based off an additional segment calculated from earlier
             angle += (360f / segments);
         }
     }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
--- Project_Implementation_Code/Curves - Rhea Mae's Code/EllipseT.cs
+++ Project_Implementation_Code/Curves - Rhea Mae's Code/EllipseT.cs
@@ -1,92 +1,106 @@
-using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
+public class EllipseT : MonoBehaviour {
 
-public class EllipseT : MonoBehaviour
+    // Equation for an Ellipse
+    // 1 = (((x-h)^2)/a^2) + (((y-v)^2)/b^2)
 
-// Equation for an Ellipse
-// 1 = (((x-h)^2)/a^2) + (((y-v)^2)/b^2)
-
-{
-	// Number of times object in placed on Ellipse
+    // Number of times object in placed on Ellipse
     private int segments = 60;
-	// Width of the Object
+    // Width of the Object
     public float objectWidth = 1;
-	// User Input Variables
+    // User Input Variables
     public float xoffset = 5; // h of equation
     public float yoffset = 5; // v of equation
     public float zoffset = 0;
     public float shrink = 1;
-	// Radius of the Ellipse
+    // Radius of the Ellipse
     public float xradius; // a of equation
     public float yradius; // b of equation
-	// List of Objects
+    // List of Objects
     public List<GameObject> cube;
-	// Some Variable
+    // Some Variable
     private LineRenderer line;
 
-    void Start()
-    {
-		// Calculating number of spaces an object will be placed at on Ellipse
-		// segments = (360) / (asin(objectWidth/radius)) * 180/3.1415 
-        segments = (int)(360 / (Mathf.Asin(objectWidth / (xradius)) * 180.0 / 3.1415));
+    // Use this for initialization
+    void Start () {
+
+        // Calculating number of spaces an object will be placed at on Ellipse
+        
+        // segments = (360) / (asin(objectWidth/radius)) * 180/3.1415 
+        //segments = (int)(360 / (Mathf.Asin(objectWidth / ((xradius + yradius) / 2)) * (180.0 / 3.1415)));
+
+        // segements = perimeter of Ellipse / objectWidth
+        int segs = (int)(((2 * 3.1415) * Mathf.Sqrt(((xradius * xradius) + (yradius * yradius)) / 2)) / objectWidth);
+
+        segments = (5*segs) / 4;
+
+        Debug.Log("Segments: " + segments);
+
         line = gameObject.GetComponent<LineRenderer>();
         line.positionCount = (segments + 1);
         line.useWorldSpace = false;
-		
-		// Calling function to place objects along Ellipse
+
+        // Calling function to place objects along Ellipse
         CreatePoints();
+
     }
 
+    void CreatePoints() {
 
-    void CreatePoints()
-    {
         // Create a new list for objects
         cube = new List<GameObject>(segments);
-		// Creating a GameObject for the object being used
+        // Creating a GameObject for the object being used
         GameObject tempCube;
-		// Physical variables to use to define each object along the Ellipse
+        // Physical variables to use to define each object along the Ellipse
         float x; // x of equation
         float y; // y of equation
         float z = 0f;
-		// Initial angle for the object
+        // Initial angle for the object
         float angle = 20f;
 
-		// Loop going through Ellipse based off number of segments calculated earlier
+        // Loop going through Ellipse based off number of segments calculated earlier
         for (int i = 0; i < (segments + 1); i++)
         {
-			// Calculating x and y of an object based off angle and radius of the Ellipse
-			// x = a*cos(angle)
-			// y = b*sin(angle)
-			// NOTE: Might have to be flipped like for the CircleT code, still unsure exactly why
-            x = xradius * Mathf.Cos(Mathf.Deg2Rad * angle) *  + xoffset;
+            // Calculating x and y of an object based off angle and radius of the Ellipse
+            // x = a*cos(angle)
+            // y = b*sin(angle)
+            // NOTE: Might have to be flipped like for the CircleT code, still unsure exactly why
+            x = xradius * Mathf.Cos(Mathf.Deg2Rad * angle) + xoffset;
             y = yradius * Mathf.Sin(Mathf.Deg2Rad * angle) + yoffset;
 
-			// Setting position calculated
+            // Setting position calculated
             line.SetPosition(i, new Vector3(x, y, z));
-			// Printing out x position of object
+            // Printing out x position of object
             Debug.Log(line.GetPosition(i).x);
-			
-			// Creating a cube as the GameObject being used
+
+            // Creating a cube as the GameObject being used
             tempCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             /*tempCube.AddComponent<Rigidbody>();*/
-			
-			// Placing object at calculated position
+
+            // Placing object at calculated position
             tempCube.transform.position = new Vector3(line.GetPosition(i).x, line.GetPosition(i).y, line.GetPosition(i).z);
-            
-			// Calculating angle of the object of the Ellipse
-			// angle = (a*tan(a*y)) / (b*x)
-			float xAngle = (xradius * Mathf.Tan(xradius * (line.GetPosition(i).y - yoffset))) / (yradius * (line.GetPosition(i).x - xoffset)) * (float)(180.0 / 3.1415);
-			
-			// Rotating object based off calculated angle from earlier
-			tempCube.transform.localEulerAngles = new Vector3(0,0,xAngle);
-            
-			// Placing cube..?
+
+            // Calculating angle of the object of the Ellipse
+            float xAngle = Mathf.Acos((line.GetPosition(i).x - xoffset) / xradius) * (float)(180.0 / 3.1415) * (line.GetPosition(i).y - xoffset) / Mathf.Abs((line.GetPosition(i).y - xoffset));
+            Debug.Log("Angle:" + xAngle);
+
+            // Rotating object based off calculated angle from earlier
+            tempCube.transform.localEulerAngles = new Vector3(0, 0, xAngle);
+
+            // Placing cube..?
             cube.Insert(i, tempCube);
-			
-			// Calculating next angle based off an additional segment calculated from earlier
+
+            // Calculating next angle based off an additional segment calculated from earlier
             angle += (360f / segments);
+            //angle += xAngle;
         }
     }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
